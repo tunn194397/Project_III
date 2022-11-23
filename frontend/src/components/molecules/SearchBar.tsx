9743913a9bfa85936da1ff61554277f5191487a8
tunn194397 <nguyenngoctu.qc2512@gmail.com>
2022-11-23 13:36:57 +0700

    TuNN: update FE for Manager Web

--- frontend/src/components/molecules/SearchBar.tsx
+++
@@ -1,8 +0,0 @@
-export default function SearchBar() {
-  return (
-    <div className='p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-gray-700 text-white'>
-      <i className='bi bi-search text-sm'></i>
-      <input type='text' placeholder='Search' className='text-[15px] ml-4 w-full bg-transparent focus:outline-none' />
-    </div>
-  );
-}
